680def6770f10a4dd6696092a9c2bd52fd663cfd
Python5668 <48457709+Python5668@users.noreply.github.com>
2019-03-29 17:58:33 -0600

    Add files via upload

--- SinglyLinkedLists_Complete.py
+++ SinglyLinkedLists_Complete.py
@@ -1,183 +1,222 @@
 """
 Lecture(s): 29, 30, & 31 combined
-This covers Linked lists and various functions that can be utilized for linked lists
+This covers Linked Lists and various functins that can be utilized for linked lists
 """
 ## Lecture 29
 class Node:
-	def __init__(self, data):
-		self.data = data # kind of like 'int info'
-		self.next = None # Setting the node pointing to null
-
+    def __init__(self, data):
+        self.data = data # kind of like 'int info'
+        self.next = None # Setting the node pointing to null
+        
 class linkedList:
-	def __init__(self):
-		self.head = None # Setting the head pointer pointing to null
+    def __init__(self):
+        self.head = None # Setting the head pointer pointing to null
+        
+    def append(self, item):
+        newNode = Node(item) # creating a new node
+        if self.head == None:
+            self.head = newNode
+            return
+        else:
+            current = self.head
+            while current.next != None:
+                current = current.next
+            current.next = newNode
+            print('<', item, 'appended into the list >')
+            return
+        
+    def prepend(self, item):
+        newNode = Node(item)
+        if self.head == None:
+            self.head = newNode
+            return
+        else:
+            newNode.next = self.head
+            self.head = newNode
+            print('<' ,item, 'prepended into the list >')
+            return
+        
+    def printList(self):
+        if self.head == None:
+            print('The list is empty!')
+        else:       
+            current = self.head
+            print('Start:')
+            while current != None:
+                print(current.data)
+                current = current.next
+            print('Finish')
 
-	def append(self, data):
-		newNode = Node(data) # Creating a new node
-		if self.head == None: # Case one, if the head pointer is pointing to null (list is empty)
-			self.head = newNode # setting the head pointer to the newNode
-			return
-		else: # Case two
-			lastNode = self.head # Make a duplicate of the head pointer
-			while lastNode.next != None: # If the pointer is still not reaching the real 'lastNode' yet, just keep moving it forward
-				lastNode = lastNode.next
-			lastNode.next = newNode
-			print(data, 'appended onto the list')
-		# Once lastNode pointer reaches the real 'lastNode', connect the lastNode.next pointer to the newNode to complete the 'Append' method
+    def deleteNode(self, key):
+        current = self.head
+        if current != None and current.data == key:
+            self.head = current.next
+            current = None
+            print('<', key, 'deleted >')
+            return
+        else:
+            prev = None
+            while current != None and current.data != key:
+                prev = current
+                current = current.next
+            if current == None:
+                print('Item was not found in list!')
+                return
+            else:
+                prev.next = current.next
+                current = None
+                print('<', key, 'deleted >')
+## Lecture 30
+    def deleteAtPos(self, pos):
+        current = self.head
+        if pos == 0:
+            self.head = current.next
+            current = None
+            print('< 1st item deleted >')
+            return
+        else:
+            cnt = 0
+            prev = None
+            while current != None and cnt != pos:
+                prev = current
+                current = current.next
+                cnt += 1
+            if current == None:
+                print('Invalid position!')
+                return
+            else:
+                print(current.data, 'has been deleted from the list')
+                prev.next = current.next
+                current = None
+                return
 
-	def prepend(self, data):
-		newNode = Node(data)
-		if self.head == None:
-			self.head = newNode
-			return
-		else:
-			newNode.next = self.head # Points the new node to the old head
-			self.head = newNode # resets the head pointer to the new node
-			print(data, 'prepended onto the list')
+    def len_iterative(self):
+        cnt = 0
+        current = self.head
+        while current != None:
+            current = current.next
+            cnt += 1
+        print('Length currently is:', cnt)
+        return
 
-	def insertAfterNode(self, prevNode, data):
-		if self.head == None:
-			print(); print("The list is empty, so there's nothing to insert after"); print()
-		else:
-			newNode = Node(data)
-			newNode.next = prevNode.next # points to the node infront of the one you want to insert after
-			prevNode.next = newNode # resets the pointer to point to the newNode
-			print(data,  'added after another node')
-
-	def printList(self):
-		if self.head == None:
-			print(); print('Cannot print an empty list'); print()
-		else:
-			curNode = self.head
-			print(); print('List Begins')
-			while curNode != None:
-				print(curNode.data)
-				curNode = curNode.next
-			print('List Ends'); print()
-
-	def deleteNode(self, key):
-		curNode = self.head # make a duplicate of the head pointer
-		if curNode != None and curNode.data == key:
-			self.head = curNode.next
-			curNode = None
-			print(key,'Deleted')
-			return
-		else:
-			prev = None
-			while curNode != None and curNode.data != key:
-				prev = curNode
-				curNode = curNode.next
-			if curNode == None:
-				print(key, 'was not found in the list'); print()
-				return
-			else:
-				prev.next = curNode.next
-				curNode = None
-				print(key, 'Deleted')
-
-## Lecture 30				
-	def deleteAtPos(self, pos):
-		curNode = self.head
-		if pos == 0:
-			self.head = curNode.next
-			curNode = None
-			print("Deleted the first position")
-			return
-		else:
-			cnt = 0
-			prev = None
-			while curNode != None and cnt != pos:
-				prev = curNode
-				curNode = curNode.next
-				cnt += 1
-			if curNode == None:
-				print("The node doesn't exsist or the position exceeds the length of the list") # Maybe the index is longer than the list
-				return
-			else:
-				print("Entry", curNode.data, "at Position", cnt, "has been deleted")
-				prev.next = curNode.next
-				curNode = None
-				
-	def len_iterative(self):
-		cnt = 0
-		curNode = self.head
-		while curNode != None:
-			curNode = curNode.next
-			cnt += 1
-		return cnt
-	def len_recursive(self, headNode):
-        	if headNode is None:
-            		return 0
-        	else:
-            		return 1+self.len_recursive(headNode.next)
-	
-	def swapNode(self, key1, key2):
-		if key1 == key2:
-			print("The two nodes are the same, cannot be swapped")
-			return
-		else:
-			prev1 = None
-			curNode1 = self.head
-			while curNode1 != None and curNode1.data != key1:
-				prev1 = curNode1
-				curNode1 = curNode1.next
-
-			prev2 = None
-			curNode2 = self.head
-			while curNode2 != None and curNode2.data != key2:
-				prev2 = curNode2
-				curNode2 = curNode2.next
-		
-			if curNode1 == None and curNode2 == None:
-				print("These nodes don't exsist in the list")
-				return
-			else:
-				if prev1 == None: # key1 is in the head node, key2 is not
-					self.head = curNode2 # red
-					prev2.next = curNode1 # black
-				elif prev2 == None: # key2 is in the head node, key1 is not
-					self.head = curNode1
-					prev1.next = curNode2
-				else: # None are the head nodes
-					prev1.next = curNode2
-					prev2.next = curNode1
-
-				# Sawp the .next pointer
-				temp1 = curNode1.next
-				temp2 = curNode2.next
-				curNode1.next = temp2
-				curNode2.next = temp1
-
-	def reverse_interative(self):
-		prev = None
-		curNode = self.head
-		while curNode != None:
-			nxt_temp = curNode.next
-			curNode.next = prev # flip the .next point to point to the front 
-			# Think about why it is not  prev = curNode.nex...
-			prev = curNode
-			curNode = nxt_temp
-		self.head = prev
-		print('List reversed')
+    def swapNode(self, key1, key2):
+        if key1 == key2:
+            print("Don't need to swap identical values")
+            return
+        else:
+            prev1 = None
+            current1 = self.head
+            while current1 != None and current1.data != key1:
+                prev1 = current1
+                current1 = current1.next
+            
+            prev2 = None
+            current2 = self.head
+            while current2 != None and current2.data != key2:
+                prev2 = current2
+                current2 = current2.next
+                
+            if current1 == None and current2 == None:
+                print('One or both of these nodes were not found in the list!')
+                return
+            else:
+                if prev1 == None:
+                    self.head = current2
+                    prev2.next = current1
+                elif prev2 == None:
+                    self.head = current1
+                    prev1.next = current2
+                else:
+                    prev1.next = current2
+                    prev2.next = current1
+                    
+                temp1 = current1.next
+                temp2 = current2.next
+                current1.next = temp2
+                current2.next = temp1
+                
+    def reverse_interative(self):
+        prev = None
+        current = self.head
+        while current != None:
+            nxt_temp = current.next
+            current.next = prev
+            prev = current
+            current = nxt_temp
+        self.head = prev
+        print('List reversed')
+## Lecture 31
+    def remove_duplicates(self):
+        prev = None
+        current = self.head
+        data_freq = dict()
+        while current != None:
+            if current.data not in data_freq:
+                data_freq[current.data] = 1
+                prev = current
+                current = current.next
+            else:
+                prev.next = current.next
+                current = None
+            current = prev.next
+            
+    def print_nth_from_last(self, n): # Fixed
+        total_len = self.len_iterative()
+        distance = total_len -1
+        current = self.head
+        while current != None:
+            if distance == n-1:
+                print(current.data)
+                return
+            else:
+                distance -= 1
+                current = current.next
+    
+    def occurances(self, data): # Fixed
+        cnt = 0
+        current = self.head
+        while current != None:
+            if current.data == data:
+                cnt += 1
+            current = current.next
+        return cnt
+    
+    def rotate(self, k): # Still buggy
+        p = self.head
+        q = self.head
+        prev = None
+        prev2 = None
+        cnt = 0
+        while p != None and cnt < k:
+            prev = p
+            p = p.next
+            cnt += 1
+        p = prev
+        
+        while q != None:
+            prev2 = q
+            q = q.next
+        q = prev2
+        
+        self.head = p.next
+        q.next = prev
+        prev = self.head
+        prev.next = p
+        
+    def tail_to_head(self): # Still buggy, I wasn't able to figure out how to get the tail's next to point to the head without getting an error.
+        lastNode = self.head
+        secondLast = None
+        while lastNode.next != None:
+            secondLast = lastNode
+            lastNode = lastNode.next
+        secondLast.next = self.head
+        self.head = lastNode
+        secondLast.next = None
+        
 
 lst = linkedList()
-lst.append('A')
-lst.append('B')
-lst.append('C')
-lst.prepend('D')
+lst.append('A'); lst.append('B'); lst.append('C'); lst.append('D')
 lst.printList()
-#lst.deleteNode('C')
-#lst.deleteAtPos(3)
-#print("The length of the list:", lst.len_iterative())
-#print("The length of the list:", lst.len_recursive(lst.head))
-print("1. swap A and C"); lst.swapNode('A','C')
+lst.tail_to_head()
+#lst.swapNode(1,8)
 lst.printList()
-print("2. swap A and A"); lst.swapNode('A','A')
-lst.printList()
-print("3. swap E and F"); lst.swapNode('E','F')
-lst.printList()
-print("4. swap B and C"); lst.swapNode('B','C')
-lst.printList()
-print("5. swap A and B"); lst.swapNode('A','B')
-lst.printList()
-#lst.reverse_interative()
